dd43b1e79a0706b346dc3986488b730dac87a348
URK21CS2037 <109648094+URK21CS2037@users.noreply.github.com>
2022-10-04 22:16:39 +0530

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,43 +1,31 @@
-class Node:
-  def __init__(self, data):
-    self.data = data
-    self.next = None
-
+class Node: 
+  def __init__(self, data): 
+    self.data = data 
+    self.next = None 
 
 class Queue:
-  def __init__(self):
-    self.head = None
-    self.last = None
-
-  def enqueue(self, data) -> None:
-    # Write your code here
-    if self.last is None:
-      self.head = Node(data)
-      self.last = self.head
+  def __init__(self): 
+    self.head = None 
+    self.last = None 
+  def enqueue(self, data) -> None: 
+    last = self.last 
+    node = Node(data) 
+    if(self.last is None and self.head is None): 
+      self.last , self.head = node , node 
     else:
-      self.last.next = Node(data)
-      self.last = self.last.next
-
+      self.last.next = node 
+      self.last = node 
   def dequeue(self) -> None:
-    # Write your code here
-    if self.head is None:
-      return None
+    if self.head == self.last:
+      self.head = self.last = None 
     else:
-      val_returned = self.head.data
-      self.head = self.head.next
-
+      self.head = self.head.next 
   def status(self) -> None:
-    # Write your code here
-    current = self.head
-    status_of_queue = []
-    while (current):
-      status_of_queue.append(current.data)
-      current = current.next
-      for element in status_of_queue:
-        print(element,end="=>")
-      print(None)
-
-
+    ptr = self.head 
+    while ptr != None:
+      print(ptr.data,end = "=>") 
+      ptr = ptr.next 
+    print("None") 
 # Do not change the following code
 queue = Queue()
 operations = []
